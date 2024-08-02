a80654342f3cd3d10b6280bd774ba1e650d93aed
Chase, Justin M <justin.chase@optum.com>
2024-08-02 11:26:18 -0500

    update the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+set -e
+
+if [[ ! $(which deno) ]]
+then
+  echo "Please install deno first!"
+  echo ""
+  echo "curl -fsSL https://deno.land/install.sh | sh"
+  echo ""
+  exit 1
+fi
+
 curl -s https://raw.githubusercontent.com/justinmchase/gh-triage/0.1.2/deno.jsonc \
   -o ~/.deno/bin/triage.deno.jsonc
 
@@ -9,3 +20,12 @@ deno install \
   --allow-run="gh,open" \
   --config ~/.deno/bin/triage.deno.jsonc \
   https://raw.githubusercontent.com/justinmchase/gh-triage/0.1.2/main.ts
+
+if [[ ! $(which triage) ]]
+then
+  echo "Installation was not successful. Please try again or report any errors here:"
+  echo ""
+  echo "https://github.com/justinmchase/gh-triage/issues"
+  echo ""
+  exit 1
+fi
