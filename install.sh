256d8f0b055bb6720d788bf92136d8d439079e61
Chase, Justin M <justin.chase@optum.com>
2024-08-02 01:25:18 -0500

    more cleanup

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-curl -s https://raw.githubusercontent.com/justinmchase/gh-triage/0.1.0/triage.deno.jsonc
+curl -s https://raw.githubusercontent.com/justinmchase/gh-triage/0.1.0/deno.jsonc \
+  -o ~/.deno/bin/triage.deno.jsonc
+
 deno install \
-    -fg \
-    -n triage \
-    --allow-net \
-    --allow-run="gh,open" \
-    --config ~/.deno/bin/triage.deno.jsonc \
-    https://raw.githubusercontent.com/justinmchase/gh-triage/0.1.0/main.ts
+  -fg \
+  -n triage \
+  --allow-net \
+  --allow-run="gh,open" \
+  --config ~/.deno/bin/triage.deno.jsonc \
+  https://raw.githubusercontent.com/justinmchase/gh-triage/0.1.0/main.ts
